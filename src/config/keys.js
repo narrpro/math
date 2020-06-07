6e5b9d05cc90766c924f16137aafc62a45ff2518
narrpro <60733221+narrpro@users.noreply.github.com>
2020-06-07 15:30:56 +0900

    Delete keys.js

--- src/config/keys.js
+++
@@ -1,8 +0,0 @@
-export default {
-  mongoURI: 'mongodb+srv://narrpro:dmswjd142857@math-test-jls8p.mongodb.net/test?retryWrites=true&w=majority',
-  pusherAppId: '974538',
-  pusherKey: '1449c5394e6b77492e22',
-  pusherSecret: 'd7a82caf5edd0f1ccd53',
-  pusherCluster: 'ap3',
-  pusherEncrypted: true
-}
