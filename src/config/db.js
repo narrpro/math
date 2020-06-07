ed12e8d50643c3216164a735f7c99d8f521e284d
narrpro <60733221+narrpro@users.noreply.github.com>
2020-06-07 15:30:48 +0900

    Delete db.js

--- src/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-import keys from '@/config/keys'
-
-mongoose.Promise = global.Promise
-mongoose
-  .connect(keys.mongoURI)
-  .then(()=> console.log('MongoDB Connected'))
-  .catch(err => console.log(err))
